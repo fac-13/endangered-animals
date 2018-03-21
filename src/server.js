c073d10e046a363a814ca0de40f25a229816e294
HA <haydnba@gmail.com>
2018-03-21 15:08:19 +0000

    add environment variable port number conditional

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@
 
 const http = require('http');
 const router = require('./router');
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(router);
 
-server.listen(3000,function(){
-  console.log('server listening')
+server.listen(PORT, function(){
+  console.log(`server listening on port: ${PORT}`)
 })
  
