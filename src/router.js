f615f1eba9e95c4c11d32cab3483b950615cea60
Jennah <contactjennah@gmail.com>
2018-03-22 10:49:34 +0000

    Add handleHome function and use request and response not req and res

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 // Import Handler Module
-const handler = require('./handler');
+const handler  = require('./handler');
 
-const router = (req, res) => {
-  let url = req.url;
+const router = (request, response) => {
+  let url = request.url;
   if (url === "/") {
-    handler.handHome(req, res);
+    handler.handleHome(request, response);
   } else if (url.indexOf("pub") !== -1) {
-    handler.handleStatic(req, res);
+    handler.handleStatic(request, response);
   } else if (url === "/get-list") {
-    handler.handleResultsList(req, res);
+    handler.handleResultsList(request, response);
   } else {
     response.writeHead(404, "Content-Type: text/html");
     response.end("<h1>404 Not Found</h1>");
