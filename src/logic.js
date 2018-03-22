3c08c29ab7e8e0c30ab414b7e84607029580dadc
HA <haydnba@gmail.com>
2018-03-22 09:57:55 +0000

    build functionality for server logic file - now passes all necessary tests Relates #3 #5

--- src/logic.js
+++ src/logic.js
@@ -1,23 +1,22 @@
 // Imports
 
+// Server-Side API Request Function
 
+// Function to Filter Species Name for Autocomplete
+const filterSpecies = function(data, selector) {
+  let species = [];
 
-// Funtion to Filter Species Name for Autocomplete
-  const filterSpecies = function(input) {
-    
-    return result = input.map(function(entry) {
-      return entry.scientific_name;
-    });
-    
-  }
+  data.forEach(function(entry) {
+    species.push(entry.scientific_name.toLowerCase());
+  });
 
-  
-
+  return (result = species.filter(function(element) {
+    return selector ? element.startsWith(selector.toLowerCase()) : element;
+  }));
+};
 
 // Function to Extract Data from API Call for Animal Details
 
-
-
 // Exports
 
- module.exports = { filterSpecies };
+module.exports = { filterSpecies };
--- test.js
+++ test.js
@@ -2,7 +2,6 @@ const test = require('tape');
 const logic = require('./src/logic.js');
 const dummy = require('./test-dummy.json')
 
-console.log(logic);
 
 test("testing tape", function(t) {
   t.equal(1, 1, "one should equal one");
@@ -18,18 +17,51 @@ test("testing tape", function(t) {
 // })
 
 
-test("filterSpecies returns array", function (t) {
+test("filterSpecies returns array", function(t) {
   var actual = Array.isArray(logic.filterSpecies(dummy.result));
   var expected = true;
   t.deepEquals(actual, expected, "should return array, got: " + actual);
   t.end();
 })
 
-test("filterSpecies returns array of strings", function (t) {
+
+test("filterSpecies returns array of strings", function(t) {
   var actual = typeof logic.filterSpecies(dummy.result)[5];
   var expected = 'string';
   t.deepEquals(actual, expected, "should return array of strings, got: " + actual);
   t.end();
 })
 
+
+test("filterSpecies returns array of strings only starting with user input", function(t) {
+  var actual = logic.filterSpecies(dummy.result, 'acr');
+  var expected = [
+    "acrocephalus agricola", 
+    "acrocephalus arundinaceus", 
+    "acrocephalus dumetorum", 
+    "acrocephalus paludicola", 
+    "acrocephalus palustris", 
+    "acrocephalus schoenobaenus", 
+    "acrocephalus scirpaceus", 
+    "acroloxus lacustris", 
+    "acroloxus rossicus"
+  ];
+  t.deepEquals(actual, expected, "should return array of strings only starting with user input, got: " + actual);
+  t.end();
+})
+
+
+test("filterSpecies returns array of strings only starting with user input", function(t) {
+  var actual = logic.filterSpecies(dummy.result, 'Ans');
+  var expected = [
+    "anser albifrons", 
+    "anser anser",  
+    "anser brachyrhynchus",  
+    "anser caerulescens",  
+    "anser erythropus", 
+    "anser fabalis"
+  ];
+  t.deepEquals(actual, expected, "should return array of strings only starting with user input, got: " + actual);
+  t.end();
+})
 
