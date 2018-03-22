27212af827af6b01cc9a85688392f79f5f42d4a7
HA <haydnba@gmail.com>
2018-03-22 10:26:20 +0000

    update server logic by splitting filtering functions Relates #5

--- src/logic.js
+++ src/logic.js
@@ -2,17 +2,26 @@
 
 // Server-Side API Request Function
 
-// Function to Filter Species Name for Autocomplete
-const filterSpecies = function(data, selector) {
+// Function to Extract List of Species from API Response Array of Objects
+const extractSpecies = function(dataList) {
   let species = [];
 
-  data.forEach(function(entry) {
+  dataList.forEach(function(entry) {
     species.push(entry.scientific_name.toLowerCase());
   });
 
-  return (result = species.filter(function(element) {
-    return selector ? element.startsWith(selector.toLowerCase()) : element;
+  return species;
+};
+
+// Function to Filter Species Name for Autocomplete
+const filterSpecies = function(dataList, userSelector) {
+  
+  const speciesNames = extractSpecies(dataList);
+
+  return (result = speciesNames.filter(function(entry) {
+    return userSelector ? entry.startsWith(userSelector.toLowerCase()) : entry;
   }));
+
 };
 
 // Function to Extract Data from API Call for Animal Details
